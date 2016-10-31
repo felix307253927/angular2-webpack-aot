fa5a9b5cb7aaf6e84feb53031af65b0d79c40a94
felix <307253927@qq.com>
2016-10-31 13:40:53 +0800

    Update main.browser.aot.ts

--- src/main.browser.aot.ts
+++ src/main.browser.aot.ts
@@ -1,3 +1,4 @@
 import { platformBrowser }    from '@angular/platform-browser';
+//Ignore error here, you can also npm run ngc eliminate the error by running npm run ngc
 import { AppModuleNgFactory } from '../aot/src/app/app.module.ngfactory';
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
